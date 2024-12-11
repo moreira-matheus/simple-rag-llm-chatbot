9ae0bd2959c1060e13752ae666e1cf6941cd2bc3
Matheus Moreira <moreira.matheus3692@gmail.com>
2024-12-11 10:53:02 -0300

    add app

---
+++ ENV_VAR.sh
@@ -0,0 +1,5 @@
+export HF_HOME=~/.huggingface
+export HF_DATASETS_CACHE=$HF_HOME/datasets
+export HF_METRICS_CACHE=$HF_HOME/metrics
+export HF_TRANSFORMERS_CACHE=$HF_HOME/transformers
+export HF_TOKEN="MY_HF_API_KEY"
